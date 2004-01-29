0a7176be13c50cd91d7003f082ceacde4bb08af3
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2004-01-29 19:46:23 +0000

    Significant optimization to countParameters(); new implementation is more than 10x faster. Removed extraneous "okay" variable from verifyThatParametersAreSet() that was not required. Minor optimization to verifyThatParametersAreSet() and createParameterMapping() to declare "int i" in the "for ()" loop instead of outside it.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@166 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbc/ParameterUtils.java
+++ src/main/net/sourceforge/jtds/jdbc/ParameterUtils.java
@@ -30,279 +30,226 @@
 // SUCH DAMAGE.
 //
 
-
 package net.sourceforge.jtds.jdbc;
 
 import java.sql.*;
 import java.util.StringTokenizer;
-// import java.util.Vector;
 
-public class ParameterUtils
-{
-   public static final String cvsVersion = "$Id: ParameterUtils.java,v 1.3 2004-01-22 23:49:42 alin_sinpalean Exp $";
+public class ParameterUtils {
+    public static final String cvsVersion = "$Id: ParameterUtils.java,v 1.4 2004-01-29 19:46:23 bheineman Exp $";
 
+    /**
+     * Count the number of parameters in a prepared sql statement.
+     *
+     * @return number of parameter placeholders in sql statement.
+     * @exception SQLException thrown if there is a problem parsing statment.
+     */
+    public static int countParameters(String sql) throws SQLException {
+        if (sql == null) {
+            throw new SQLException("No statement");
+        }
 
-   /**
-    * Count the number of parameters in a prepared sql statement.
-    *
-    * @return number of parameter placeholders in sql statement.
-    * @exception SQLException thrown if there is a problem parsing statment.
-    * @todo   Find a faster implementation; use the same code for Procedure().
-    */
-   public static int countParameters(String sql)
-      throws java.sql.SQLException
-   {
-      //
-      // This is method is implemented as a very simple finite state machine.
-      //
+        char[] chars = sql.toCharArray(); // avoid getfield opcode
+        boolean inString = false;
+        int result = 0;
 
-      int result = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char ch = chars[i]; // avoid getfield opcode
 
-      if( sql == null )
-         throw new SQLException("No statement");
-      else
-      {
-         // SAfe Seems like '\' (backslash) doesn't actually work as
-         //      an escape character, so we'll just ignore it
-         StringTokenizer   st = new StringTokenizer(sql, "'?", true);
-         final int         normal   = 1;
-         final int         inString = 2;
-         final int         inEscape = 3;
-         int               state = normal;
-         String            current;
-
-         while (st.hasMoreTokens())
-         {
-            current = st.nextToken();
-            switch (state)
-            {
-               case normal:
-                  if (current.equals("?"))
-                     result++;
-                  else if (current.equals("'"))
-                     state = inString;
-
-                  break;
-
-               case inString:
-                  if (current.equals("'"))
-                     state = normal;
-                  // SAfe Seems like '\' (backslash) doesn't actually work as
-                  //      an escape character, so we'll just ignore it
-                  // else if (current.equals("\\"))
-                  //    state = inEscape;
-
-                  break;
-
-               case inEscape:
-                  state = inString;
-                  break;
-
-               default:
-                  throw new SQLException("Internal error.  Bad State " + state);
+            if (!inString) {
+                if (ch == '?') {
+                    result++;
+                } else if (ch == '\'') {
+                    inString = true;
+                }
+            } else {
+                // NOTE: This works even if a single quote is being used as an escape
+                // character since the next single quote found will force the state
+                // to be inString == true again.
+                if (ch == '\'') {
+                    inString = false;
+                }
             }
-         }
-      }
+        }
 
-      return result;
-   }
+        return result;
+    }
 
-   /**
-    * check that all items in parameterList have been given a value
-    *
-    * @exception SQLException thrown if one or more parameters aren't set
-    */
-   public static void verifyThatParametersAreSet(
-      ParameterListItem[] parameterList)
-      throws SQLException
-   {
-      int     i;
-      boolean okay = true;
+    /**
+     * Check that all items in parameterList have been given a value
+     *
+     * @exception SQLException thrown if one or more parameters aren't set
+     */
+    public static void verifyThatParametersAreSet(ParameterListItem[] parameterList)
+    throws SQLException {
+        for (int i = 0; i < parameterList.length; i++) {
+            if (parameterList[i].isOutput) {
+                // Allow output parameters to be "not set"
+                parameterList[i].isSet = true;
+            }
 
-      for(i=0; okay && i<parameterList.length; i++)
-      {
-         if (parameterList[i].isOutput)
-         {
-            // Allow output parameters to be "not set"
-            parameterList[i].isSet = true;
-         }
-         okay = okay && parameterList[i].isSet;
-         if (!okay)
-         {
-            throw new SQLException("parameter #" + (i+1)
-                                   + " has not been set");
-         }
-      }
-   }
+            if (!parameterList[i].isSet) {
+                throw new SQLException("parameter #" + (i + 1) + " has not been set");
+            }
+        }
+    }
 
-   /**
-    * create the formal parameters for a parameter list.
-    *
-    * This method takes a sql string and a parameter list containing
-    * the actual parameters and creates the formal parameters for
-    * a stored procedure.  The formal parameters will later be used
-    * when the stored procedure is submitted for creation on the server.
-    *
-    * @param rawQueryString   (in-only)
-    * @param parameterList    (update)
-    *
-    */
-   public static void createParameterMapping(
-      String              rawQueryString,
-      ParameterListItem[] parameterList,
-      Tds tds)
-      throws SQLException
-   {
-      int    i;
-      String nextFormal;
-      int    nextParameterNumber = 0;
-      int    tdsVer              = tds.getTdsVer();
-      EncodingHelper encoder     = tds.getEncoder();
+    /**
+     * Create the formal parameters for a parameter list.
+     *
+     * This method takes a sql string and a parameter list containing
+     * the actual parameters and creates the formal parameters for
+     * a stored procedure.  The formal parameters will later be used
+     * when the stored procedure is submitted for creation on the server.
+     *
+     * @param rawQueryString   (in-only)
+     * @param parameterList    (update)
+     */
+    public static void createParameterMapping(String rawQueryString,
+                                              ParameterListItem[] parameterList,
+                                              Tds tds)
+    throws SQLException {
+        String nextFormal;
+        int nextParameterNumber = 0;
+        int tdsVer = tds.getTdsVer();
+        EncodingHelper encoder = tds.getEncoder();
 
-      for( i=0; i<parameterList.length; i++ )
-      {
-         do
-         {
-            nextParameterNumber++;
-            nextFormal = "P" + nextParameterNumber;
-         }
-         while( -1 != rawQueryString.indexOf(nextFormal) );
+        for (int i = 0; i < parameterList.length; i++) {
+            do {
+                nextParameterNumber++;
+                nextFormal = "P" + nextParameterNumber;
+            }
 
-         parameterList[i].formalName = nextFormal;
+            while (-1 != rawQueryString.indexOf(nextFormal));
 
-         switch (parameterList[i].type)
-         {
-            case java.sql.Types.VARCHAR:
-            case java.sql.Types.CHAR:
-            {
-               String value = (String)parameterList[i].value;
+            parameterList[i].formalName = nextFormal;
 
-               if( tdsVer == Tds.TDS70 )
-               {
-                   // SQL Server 7 can handle Unicode so use it wherever
-                   // possible AND required
-                   if( (value==null || value.length()<4001) && tds.useUnicode() )
-                   {
-                       parameterList[i].formalType = "nvarchar(4000)";
-                       parameterList[i].maxLength = 4000;
-                   }
-                   else if( value==null ||
-                             (value.length()<8001
-                              && !encoder.isDBCS()
-                              && encoder.canBeConverted(value)) )
-                   {
-                       parameterList[i].formalType = "varchar(8000)";
-                       parameterList[i].maxLength = 8000;
-                   }
-                   else
-                   {
-                       // SAfe We'll always use NTEXT (not TEXT) because CLOB
-                       //      columns can't be index columns, so there's no
-                       //      performance hit
-                       parameterList[i].formalType = "ntext";
-                       parameterList[i].maxLength = Integer.MAX_VALUE;
-                   }
-               }
-               else
-               {
-                   int len = 0; // This is for NULL values
-                   if( value != null )
-                   {
-                       len = value.length();
-                       if( encoder.isDBCS() && len>127 && len<256 )
-                           len = encoder.getBytes(value).length;
-                   }
+            switch (parameterList[i].type) {
+                case java.sql.Types.VARCHAR:
+                case java.sql.Types.CHAR:
+                {
+                    String value = (String)parameterList[i].value;
 
-                   if( len < 256 )
-                   {
-                       parameterList[i].formalType = "varchar(255)";
-                       parameterList[i].maxLength = 255;
-                   }
-                   else
-                   {
-                       parameterList[i].formalType = "text";
-                       parameterList[i].maxLength = Integer.MAX_VALUE;
-                   }
-               }
-               break;
-            }
-            case java.sql.Types.LONGVARCHAR:
-            {
-               if( tdsVer == Tds.TDS70 )
-                   parameterList[i].formalType = "ntext";
-               else
-                   parameterList[i].formalType = "text";
+                    if (tdsVer == Tds.TDS70) {
+                        // SQL Server 7 can handle Unicode so use it wherever
+                        // possible AND required
+                        if ((value == null || value.length() < 4001)
+                            && tds.useUnicode()) {
+                            parameterList[i].formalType = "nvarchar(4000)";
+                            parameterList[i].maxLength = 4000;
+                        } else if (value == null ||
+                                    (value.length() < 8001
+                                     && !encoder.isDBCS()
+                                     && encoder.canBeConverted(value))) {
+                            parameterList[i].formalType = "varchar(8000)";
+                            parameterList[i].maxLength = 8000;
+                        } else {
+                            // SAfe We'll always use NTEXT (not TEXT) because CLOB
+                            //      columns can't be index columns, so there's no
+                            //      performance hit
+                            parameterList[i].formalType = "ntext";
+                            parameterList[i].maxLength = Integer.MAX_VALUE;
+                        }
+                    } else {
+                        int len = 0; // This is for NULL values
 
-               parameterList[i].maxLength = Integer.MAX_VALUE;
-               break;
+                        if (value != null) {
+                            len = value.length();
+
+                            if (encoder.isDBCS() && len > 127 && len < 256) {
+                                len = encoder.getBytes(value).length;
+                            }
+                        }
+
+                        if (len < 256) {
+                            parameterList[i].formalType = "varchar(255)";
+                            parameterList[i].maxLength = 255;
+                        } else {
+                            parameterList[i].formalType = "text";
+                            parameterList[i].maxLength = Integer.MAX_VALUE;
+                        }
+                    }
+                    break;
+                }
+                case java.sql.Types.LONGVARCHAR:
+                {
+                    if (tdsVer == Tds.TDS70) {
+                        parameterList[i].formalType = "ntext";
+                    } else {
+                        parameterList[i].formalType = "text";
+                    }
+
+                    parameterList[i].maxLength = Integer.MAX_VALUE;
+                    break;
+                }
+                case java.sql.Types.INTEGER:
+                {
+                    parameterList[i].formalType = "integer";
+                    break;
+                }
+                case java.sql.Types.FLOAT:
+                case java.sql.Types.REAL:
+                {
+                    parameterList[i].formalType = "real";
+                    break;
+                }
+                case java.sql.Types.DOUBLE:
+                {
+                    parameterList[i].formalType = "float";
+                    break;
+                }
+                case java.sql.Types.TIMESTAMP:
+                case java.sql.Types.DATE:
+                case java.sql.Types.TIME:
+                {
+                    parameterList[i].formalType = "datetime";
+                    break;
+                }
+                case java.sql.Types.LONGVARBINARY:
+                case java.sql.Types.VARBINARY:
+                {
+                    parameterList[i].formalType = "image";
+                    break;
+                }
+                case java.sql.Types.BIT:
+                {
+                    parameterList[i].formalType = "bit";
+                    break;
+                }
+                case java.sql.Types.BIGINT: {
+                    parameterList[i].formalType = "decimal(38,10)";
+                    break;
+                }
+                case java.sql.Types.SMALLINT:
+                {
+                    parameterList[i].formalType = "smallint";
+                    break;
+                }
+                case java.sql.Types.TINYINT:
+                {
+                    parameterList[i].formalType = "tinyint";
+                    break;
+                }
+                case java.sql.Types.DECIMAL:
+                case java.sql.Types.NUMERIC:
+                {
+                    parameterList[i].formalType = "decimal(38,10)";
+                    break;
+                }
+                case java.sql.Types.BINARY:
+                case java.sql.Types.NULL:
+                case java.sql.Types.OTHER:
+                {
+                    throw new SQLException("Not implemented (type is java.sql.Types."
+                                           + TdsUtil.javaSqlTypeToString(parameterList[i].type)
+                                           + ")");
+                }
+                default:
+                {
+                    throw new SQLException("Internal error.  Unrecognized type "
+                                           + parameterList[i].type);
+                }
             }
-            case java.sql.Types.INTEGER:
-            {
-               parameterList[i].formalType = "integer";
-               break;
-            }
-            case java.sql.Types.FLOAT:
-            case java.sql.Types.REAL:
-            {
-               parameterList[i].formalType = "real";
-               break;
-            }
-            case java.sql.Types.DOUBLE:
-            {
-               parameterList[i].formalType = "float";
-               break;
-            }
-            case java.sql.Types.TIMESTAMP:
-            case java.sql.Types.DATE:
-            case java.sql.Types.TIME:
-            {
-               parameterList[i].formalType = "datetime";
-               break;
-            }
-            case java.sql.Types.LONGVARBINARY:
-            case java.sql.Types.VARBINARY:
-            {
-               parameterList[i].formalType = "image";
-               break;
-            }
-            case java.sql.Types.BIT:
-            {
-               parameterList[i].formalType = "bit";
-               break;
-            }
-            case java.sql.Types.BIGINT: {
-               parameterList[i].formalType = "decimal(38,10)";
-               break;
-            }
-            case java.sql.Types.SMALLINT:
-            {
-               parameterList[i].formalType = "smallint";
-               break;
-            }
-            case java.sql.Types.TINYINT:
-            {
-               parameterList[i].formalType = "tinyint";
-               break;
-            }
-            case java.sql.Types.DECIMAL:
-            case java.sql.Types.NUMERIC:
-            {
-               parameterList[i].formalType = "decimal(38,10)";
-               break;
-            }
-            case java.sql.Types.BINARY:
-            case java.sql.Types.NULL:
-            case java.sql.Types.OTHER:
-            {
-               throw new SQLException("Not implemented (type is java.sql.Types."
-                                      + TdsUtil.javaSqlTypeToString(parameterList[i].type) + ")");
-            }
-            default:
-            {
-               throw new SQLException("Internal error.  Unrecognized type "
-                                      + parameterList[i].type);
-            }
-         }
-      }
-   }
+        }
+    }
 }

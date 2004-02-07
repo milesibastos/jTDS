72ee400893caf83568eeb03bae95467d8b8cc97c
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2004-02-07 19:27:23 +0000

    Simple test additions.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@204 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/test/net/sourceforge/jtds/test/CallableStatementTest.java
+++ src/test/net/sourceforge/jtds/test/CallableStatementTest.java
@@ -6,25 +6,116 @@ import junit.framework.TestCase;
 /**
  * @version 1.0
  */
-
-public class CallableStatementTest
-extends TestBase
-{
-
-    public CallableStatementTest( String name )
-    {
+public class CallableStatementTest extends TestBase {
+    public CallableStatementTest(String name) {
         super(name);
     }
 
-    public void testCallableStatement()
-        throws Exception
-    {
-        //For now, just test that we can create a CallableStatement
-        CallableStatement stmt = con.prepareCall( "THIS IS A TEST ONLY" );
+    public void testCallableStatement() throws Exception {
+        CallableStatement stmt = con.prepareCall("{call sp_who}");
+
+        stmt.close();
     }
 
-    public static void main(String[] args)
-    {
-        junit.textui.TestRunner.run( CallableStatementTest.class );
+    public void testCallableStatementCall1() throws Exception {
+        CallableStatement stmt = con.prepareCall("{call sp_who}");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public void testCallableStatementExec1() throws Exception {
+        CallableStatement stmt = con.prepareCall("exec sp_who");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public void testCallableStatementExec2() throws Exception {
+        CallableStatement stmt = con.prepareCall("EXEC sp_who");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public void testCallableStatementExec3() throws Exception {
+        CallableStatement stmt = con.prepareCall("execute sp_who");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public void testCallableStatementExec4() throws Exception {
+        CallableStatement stmt = con.prepareCall("EXECUTE sp_who");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public void testCallableStatementExec5() throws Exception {
+        CallableStatement stmt = con.prepareCall("eXeC sp_who");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public void testCallableStatementExec6() throws Exception {
+        CallableStatement stmt = con.prepareCall("ExEcUtE sp_who");
+
+        makeTestTables(stmt);
+        makeObjects(stmt, 8);
+
+        ResultSet rs = stmt.executeQuery();
+
+        dump(rs);
+
+        stmt.close();
+        rs.close();
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(CallableStatementTest.class);
     }
 }

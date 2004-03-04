60dac8f81b29692b616a5d26166409c9e19e9357
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2004-03-04 04:56:08 +0000

    Updated logic to deal with whitespaces properly.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@258 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbc/CallableStatement_base.java
+++ src/main/net/sourceforge/jtds/jdbc/CallableStatement_base.java
@@ -64,11 +64,11 @@ import java.util.Calendar;
  *
  * @see  Connection#prepareCall
  * @see  ResultSet
- * @version  $Id: CallableStatement_base.java,v 1.17 2004-03-04 04:30:20 bheineman Exp $
+ * @version  $Id: CallableStatement_base.java,v 1.18 2004-03-04 04:53:42 bheineman Exp $
  */
 public class CallableStatement_base extends PreparedStatement_base
 implements java.sql.CallableStatement {
-    public final static String cvsVersion = "$Id: CallableStatement_base.java,v 1.17 2004-03-04 04:30:20 bheineman Exp $";
+    public final static String cvsVersion = "$Id: CallableStatement_base.java,v 1.18 2004-03-04 04:53:42 bheineman Exp $";
 
     private String procedureName = null;
     private boolean lastWasNull = false;
@@ -121,13 +121,15 @@ implements java.sql.CallableStatement {
         for (; i < length; i++) {
             char ch = rawQueryString.charAt(i);
 
-            if (Character.isLetterOrDigit(ch)
-                || ch == '#'
-                || ch == '_'
-                || ch == '?') {
-                break;
-            } else if (ch == '[' || ch == '"') {
+            if (ch == '[' || ch == '"') {
                 inString = true;
+                break;
+            } else if (ch != ' ' // space
+                       && ch != '\t' // tab
+                       && ch != '\n' // newline
+                       && ch != '\r' // carriage-return
+                       && ch != '\f') { // form-feed
+                // A non ANSI SQL-92 whitespace character was found
                 break;
             }
         }
@@ -165,10 +167,12 @@ implements java.sql.CallableStatement {
             } else {
                 if (ch == '[' || ch == '"') {
                     inString = true;
-                } else if (!Character.isLetterOrDigit(ch)
-                    && ch != '#'
-                    && ch != '_'
-                    && ch != '.') {
+                } else if (ch == ' ' // space
+                           || ch == '\t' // tab
+                           || ch == '\n' // newline
+                           || ch == '\r' // carriage-return
+                           || ch == '\f') { // form-feed
+                    // An ANSI SQL-92 whitespace character was found
                     break;
                 }
             }

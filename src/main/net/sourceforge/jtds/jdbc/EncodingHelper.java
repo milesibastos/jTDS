ddfba9f0ba5b18c729e8dff784d0af4776cef24c
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2004-04-02 18:34:27 +0000

    Cleaned up EncodingHelper in preperation for Clob stream support; removed initialization logic for Microsofts VM (which jTDS is no longer compatible with as it uses Java 2 classes).
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@304 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbc/EncodingHelper.java
+++ src/main/net/sourceforge/jtds/jdbc/EncodingHelper.java
@@ -7,83 +7,61 @@ import java.util.Hashtable;
  * Helper class to handle server character set conversion.
  *
  * @author Stefan Bodewig <a href="mailto:stefan.bodewig@megabit.net">stefan.bodewig@megabit.net</a>
- * @version  $Id: EncodingHelper.java,v 1.4 2003-12-16 19:08:48 alin_sinpalean Exp $
+ * @version  $Id: EncodingHelper.java,v 1.5 2004-04-02 18:34:14 bheineman Exp $
  */
 public class EncodingHelper
 {
-    public static final String cvsVersion = "$Id: EncodingHelper.java,v 1.4 2003-12-16 19:08:48 alin_sinpalean Exp $";
+    public static final String cvsVersion = "$Id: EncodingHelper.java,v 1.5 2004-04-02 18:34:14 bheineman Exp $";
 
     /**
      * Array containig the bytes 0x00 - 0xFF.
      */
-    private static byte[] convArray;
+    private static byte[] convArray = new byte[256];
     /**
      * Hashtable holding instances for all known encodings.
      */
-    private static Hashtable knownEncodings;
-    /**
-     * Simple boolean to ensure we initialize once and only once.
-     */
-    private static boolean initialized;
+    private static Hashtable knownEncodings = new Hashtable();
 
     /**
      * The name of the encoding.
      */
     private String name;
+
     /**
      * Is this a DBCS charset (does it need more than one byte per character)?
      */
     private boolean wideChars;
 
-    static
-    {
-        initialize();
-    }
-
-    /**
-     * Initialize the static variables.
-     * <p>
-     * Will be called from the static block above, but some VMs (notably
-     * Microsoft's) won't run this.
-     */
-    private static void initialize()
-    {
-        convArray = new byte[256];
-        for( int i=0; i<256; i++ )
-            convArray[i] = (byte)i;
-
-        knownEncodings = new Hashtable();
-        // SAfe: SQL Server returns iso_1, but it's actually Cp1252
-        try
-        {
-            knownEncodings.put("iso_1", new EncodingHelper("Cp1252"));
+    static {
+        for (int i = 0; i < 256; i++) {
+            convArray[i] = (byte) i;
         }
-        catch( UnsupportedEncodingException ex )
-        {}
-        initialized = true;
+
+        // SAfe: SQL Server returns iso_1, but it's actually Cp1252
+        try {
+            knownEncodings.put("iso_1", new EncodingHelper("Cp1252"));
+        } catch (UnsupportedEncodingException e) {
+        }
     }
+
 
     /**
      * private so only the static accessor can be used.
      */
-    private EncodingHelper(String name) throws UnsupportedEncodingException
-    {
+    private EncodingHelper(String name) throws UnsupportedEncodingException {
         this.name = name;
+
         // SAfe Hope this works in all cases.
-        wideChars = new String(convArray, name).length()!=convArray.length;
+        wideChars = new String(convArray, name).length() != convArray.length;
     }
 
     /**
      * Translate the String into a byte[] in the server's encoding.
      */
-    public byte[] getBytes(String value)
-    {
-        try
-        {
+    public byte[] getBytes(String value) {
+        try {
             return value.getBytes(name);
-        }
-        catch( UnsupportedEncodingException uee )
-        {
+        } catch (UnsupportedEncodingException e) {
             return value.getBytes();
         }
     }
@@ -91,8 +69,7 @@ public class EncodingHelper
     /**
      * Translate the byte[] from the server's encoding to a Unicode String.
      */
-    public String getString(byte[] value)
-    {
+    public String getString(byte[] value) {
         return getString(value, 0, value.length);
     }
 
@@ -103,14 +80,10 @@ public class EncodingHelper
      * The subarray starting at index off and extending to off+len-1
      * is translated.
      */
-    public String getString(byte[] value, int off, int len)
-    {
-        try
-        {
+    public String getString(byte[] value, int off, int len) {
+        try {
             return new String(value, off, len, name);
-        }
-        catch( UnsupportedEncodingException uee )
-        {
+        } catch (UnsupportedEncodingException e) {
             return new String(value, off, len);
         }
     }
@@ -118,22 +91,20 @@ public class EncodingHelper
     /**
      * Is this a DBCS charset (does it need more than one byte per character)?
      */
-    public boolean isDBCS() {return wideChars;}
+    public boolean isDBCS() {
+        return wideChars;
+    }
 
     /**
      * Can the given String be converted to the server's charset?
      */
-    public boolean canBeConverted(String value)
-    {
-        try
-        {
+    public boolean canBeConverted(String value) {
+        try {
             // SAfe Not very efficient, since a String is always created, but it's the only way I
             //      could think of. Maybe some Reader/Writer combination would have been better
             //      but...
             return new String(value.getBytes(name), name).equals(value);
-        }
-        catch( UnsupportedEncodingException ex )
-        {
+        } catch (UnsupportedEncodingException e) {
             // SAfe No way.
             return false;
         }
@@ -142,43 +113,40 @@ public class EncodingHelper
     /**
      * Return the helper object for the given encoding.
      */
-    public static EncodingHelper getHelper(String encodingName)
-    {
-        if( !initialized )
-            synchronized( net.sourceforge.jtds.jdbc.EncodingHelper.class )
-            {
-                if( !initialized )
-                    initialize();
+    public static EncodingHelper getHelper(String encodingName) {
+        EncodingHelper res = (EncodingHelper) knownEncodings.get(encodingName);
+
+        if (res == null) {
+            try {
+                res = new EncodingHelper(encodingName);
+            } catch (UnsupportedEncodingException e) {
             }
-
-        EncodingHelper res = (EncodingHelper)knownEncodings.get(encodingName);
-
-        if( res == null )
-        {
-            try { res = new EncodingHelper(encodingName); }
-            catch( UnsupportedEncodingException ex ) { }
         }
+
         // SAfe Try prepending Cp or MS to the character set. This should
         //      cover all the character sets known by SQL Server.
-        if( res == null )
-        {
-            try { res = new EncodingHelper("Cp"+encodingName); }
-            catch( UnsupportedEncodingException ex ) { }
-        }
-        if( res == null )
-        {
-            try { res = new EncodingHelper("MS"+encodingName); }
-            catch( UnsupportedEncodingException ex ) { }
+        if (res == null) {
+            try {
+                res = new EncodingHelper("Cp" + encodingName);
+            } catch (UnsupportedEncodingException e) {
+            }
         }
 
-        if( res != null )
+        if (res == null) {
+            try {
+                res = new EncodingHelper("MS" + encodingName);
+            } catch (UnsupportedEncodingException e) {
+            }
+        }
+
+        if (res != null) {
             knownEncodings.put(encodingName, res);
+        }
 
         return res;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 }

06d99cc2241056f445a63093734f79826a83c946
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2004-08-28 15:01:34 +0000

    o Corrected bug [1017957] 9.1 rc1 and resin 3.0.8 incompatibilities?;    JtdsDataSource no longer inherits accessors and mutators from another class.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@532 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbcx/JtdsDataSource.java
+++ src/main/net/sourceforge/jtds/jdbcx/JtdsDataSource.java
@@ -17,19 +17,52 @@
 //
 package net.sourceforge.jtds.jdbcx;
 
+import java.io.PrintWriter;
+import java.io.Serializable;
 import java.sql.*;
 import java.util.*;
 
+import javax.naming.NamingException;
+import javax.naming.Reference;
+import javax.naming.Referenceable;
+import javax.naming.StringRefAddr;
+import javax.sql.DataSource;
+
 import net.sourceforge.jtds.jdbc.*;
+import net.sourceforge.jtds.jdbc.Driver;
+import net.sourceforge.jtds.util.Logger;
 
 /**
  * A plain <code>DataSource</code> implementation.
  *
  * @author Alin Sinplean
  * @since  jTDS 0.3
- * @version $Id: JtdsDataSource.java,v 1.12 2004-08-24 17:45:07 bheineman Exp $
+ * @version $Id: JtdsDataSource.java,v 1.13 2004-08-28 15:01:34 bheineman Exp $
  */
-public class JtdsDataSource extends AbstractDataSource {
+public class JtdsDataSource
+implements DataSource, Referenceable, Serializable {
+    protected int loginTimeout = DefaultProperties.LOGIN_TIMEOUT;
+    protected String databaseName = DefaultProperties.DATABASE_NAME;
+    protected int portNumber = DefaultProperties.PORT_NUMBER_SQLSERVER;
+    protected String serverName;
+    protected String user = DefaultProperties.USER;
+    protected String password = DefaultProperties.PASSWORD;
+    protected String description;
+    protected String tdsVersion = DefaultProperties.TDS_VERSION_70;
+    protected int serverType = Driver.SQLSERVER;
+    protected String charset = "";
+    protected String language = "";
+    protected String domain = "";
+    protected String instance = "";
+    protected boolean lastUpdateCount = DefaultProperties.LAST_UPDATE_COUNT;
+    protected boolean sendStringParametersAsUnicode = DefaultProperties.USE_UNICODE;
+    protected boolean namedPipe = DefaultProperties.NAMED_PIPE;
+    protected String macAddress = DefaultProperties.MAC_ADDRESS;
+    protected int packetSize = DefaultProperties.PACKET_SIZE_70_80;
+    protected int prepareSql = DefaultProperties.PREPARE_SQL;
+    protected long lobBuffer = DefaultProperties.LOB_BUFFER_SIZE;
+    protected String appName = DefaultProperties.APP_NAME;
+    protected String progName = DefaultProperties.PROG_NAME;
 
     /**
      * Constructs a new datasource.
@@ -86,5 +119,230 @@ public class JtdsDataSource extends AbstractDataSource {
         String url = "jdbc:jtds:" + DefaultProperties.getServerType(serverType) + ":";
         
         return driver.connect(url, props);
+    }
+
+    public Reference getReference() throws NamingException {
+        Reference ref = new Reference(getClass().getName(),
+                                      JtdsObjectFactory.class.getName(),
+                                      null);
+
+        ref.add(new StringRefAddr(Messages.get("prop.servername"), serverName));
+        ref.add(new StringRefAddr(Messages.get("prop.portnumber"),
+                                  String.valueOf(portNumber)));
+        ref.add(new StringRefAddr(Messages.get("prop.databasename"), databaseName));
+        ref.add(new StringRefAddr(Messages.get("prop.user"), user));
+        ref.add(new StringRefAddr(Messages.get("prop.password"), password));
+        ref.add(new StringRefAddr(Messages.get("prop.charset"), charset));
+        ref.add(new StringRefAddr(Messages.get("prop.language"), language));
+        ref.add(new StringRefAddr(Messages.get("prop.tds"), tdsVersion));
+        ref.add(new StringRefAddr(Messages.get("prop.servertype"),
+                                  String.valueOf(serverType)));
+        ref.add(new StringRefAddr(Messages.get("prop.domain"), domain));
+        ref.add(new StringRefAddr(Messages.get("prop.instance"), instance));
+        ref.add(new StringRefAddr(Messages.get("prop.lastupdatecount"),
+                                  String.valueOf(getLastUpdateCount())));
+        ref.add(new StringRefAddr(Messages.get("prop.logintimeout"),
+                                  String.valueOf(loginTimeout)));
+        ref.add(new StringRefAddr(Messages.get("prop.useunicode"),
+                                  String.valueOf(sendStringParametersAsUnicode)));
+        ref.add(new StringRefAddr(Messages.get("prop.namedpipe"),
+                                  String.valueOf(namedPipe)));
+        ref.add(new StringRefAddr(Messages.get("prop.macaddress"), macAddress));
+        ref.add(new StringRefAddr(Messages.get("prop.packetsize"),
+                                  String.valueOf(packetSize)));
+        ref.add(new StringRefAddr(Messages.get("prop.preparesql"),
+                                  String.valueOf(prepareSql)));
+        ref.add(new StringRefAddr(Messages.get("prop.lobbuffer"),
+                                  String.valueOf(lobBuffer)));
+        ref.add(new StringRefAddr(Messages.get("prop.appname"),
+                                  String.valueOf(appName)));
+        ref.add(new StringRefAddr(Messages.get("prop.progname"),
+                                  String.valueOf(progName)));
+
+        return ref;
+    }
+    
+    public PrintWriter getLogWriter() throws SQLException {
+        return Logger.getLogWriter();
+    }
+  
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        Logger.setLogWriter(out);
+    }
+  
+    public void setLoginTimeout(int loginTimeout) throws SQLException {
+        this.loginTimeout = loginTimeout;
+    }
+  
+    public int getLoginTimeout() throws SQLException {
+        return loginTimeout;
+    }
+  
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+  
+    public String getDatabaseName() {
+        return databaseName;
+    }
+  
+    public void setDescription(String description) {
+        this.description = description;
+    }
+  
+    public String getDescription() {
+        return description;
+    }
+  
+    public void setPassword(String password) {
+        this.password = password;
+    }
+  
+    public String getPassword() {
+        return password;
+    }
+  
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
+  
+    public int getPortNumber() {
+        return portNumber;
+    }
+  
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+  
+    public String getServerName() {
+        return serverName;
+    }
+  
+    public void setUser(String user) {
+        this.user = user;
+    }
+  
+    public String getUser() {
+        return user;
+    }
+  
+    public void setTds(String tds) {
+        this.tdsVersion = tds;
+    }
+  
+    public String getTds() {
+        return tdsVersion;
+    }
+  
+    public void setServerType(int serverType) {
+        this.serverType = serverType;
+    }
+
+    public int getServerType() {
+        return serverType;
+    }
+  
+    public String getDomain() {
+        return domain;
+    }
+  
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+  
+    public String getInstance() {
+        return instance;
+    }
+  
+    public void setInstance(String instance) {
+        this.instance = instance;
+    }
+  
+    public boolean getSendStringParametersAsUnicode() {
+        return sendStringParametersAsUnicode;
+    }
+  
+    public void setSendStringParametersAsUnicode(boolean sendStringParametersAsUnicode) {
+        this.sendStringParametersAsUnicode = sendStringParametersAsUnicode;
+    }
+
+    public boolean getNamedPipe() {
+        return namedPipe;
+    }
+
+    public void setNamedPipe(boolean namedPipe) {
+        this.namedPipe = namedPipe;
+    }
+
+    public boolean getLastUpdateCount() {
+        return lastUpdateCount;
+    }
+  
+    public void setLastUpdateCount(boolean lastUpdateCount) {
+        this.lastUpdateCount = lastUpdateCount;
+    }
+    
+    public String getCharset() {
+        return charset;
+    }
+  
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+  
+    public String getLanguage() {
+        return language;
+    }
+  
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+  
+    public String getMacAddress() {
+        return macAddress;
+    }
+  
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+  
+    public void setPacketSize(int packetSize) {
+        this.packetSize = packetSize;
+    }
+  
+    public int getPacketSize() {
+        return packetSize;
+    }
+  
+    public void setPrepareSql(int prepareSql) {
+        this.prepareSql = prepareSql;
+    }
+  
+    public int getPrepareSql() {
+        return prepareSql;
+    }
+    
+    public void setLobBuffer(long lobBuffer) {
+        this.lobBuffer = lobBuffer;
+    }
+  
+    public long getLobBuffer() {
+        return lobBuffer;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setProgName(String progName) {
+        this.progName = progName;
+    }
+
+    public String getProgName() {
+        return progName;
     }
 }

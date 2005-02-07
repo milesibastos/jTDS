5a3d6f847559ca89be515ba5455c4e83b88eacfc
alin_sinpalean <alin_sinpalean@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2005-02-07 13:47:54 +0000

    o Removed tests that were failing because of the fix for bug [1113709]    Connecting via DataSource (the assumptions of these tests were incorrect, to    start with).
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@710 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/test/net/sourceforge/jtds/test/JtdsDataSourceUnitTest.java
+++ src/test/net/sourceforge/jtds/test/JtdsDataSourceUnitTest.java
@@ -19,7 +19,7 @@ import javax.naming.NamingException;
  * Unit tests for the {@link JtdsDataSource} class.
  *
  * @author David D. Kilzer
- * @version $Id: JtdsDataSourceUnitTest.java,v 1.11 2005-02-02 13:42:49 alin_sinpalean Exp $
+ * @version $Id: JtdsDataSourceUnitTest.java,v 1.12 2005-02-07 13:47:45 alin_sinpalean Exp $
  */
 public class JtdsDataSourceUnitTest extends UnitTestBase {
 
@@ -35,14 +35,6 @@ public class JtdsDataSourceUnitTest extends UnitTestBase {
     public static Test suite() {
 
         final TestSuite testSuite = new TestSuite(JtdsDataSourceUnitTest.class);
-
-        testSuite.addTest(
-                JtdsDataSourceUnitTest.Test_JtdsDataSource_fields.suite(
-                        "test_fields_DefaultProperties"));
-
-        testSuite.addTest(
-                JtdsDataSourceUnitTest.Test_JtdsDataSource_getReference.suite(
-                        "test_getReference_DefaultProperties"));
 
         testSuite.addTest(
                 JtdsDataSourceUnitTest.Test_JtdsDataSource_getConnection.suite(
@@ -69,111 +61,6 @@ public class JtdsDataSourceUnitTest extends UnitTestBase {
      */
     public void testPublicConstructor() {
         assertNotNull(new JtdsDataSource());
-    }
-
-
-
-    /**
-     * Class used to test {@link JtdsDataSource}.
-     */
-    public static class Test_JtdsDataSource_fields
-            extends DefaultPropertiesTestLibrary {
-
-        /**
-         * Construct a test suite for this library.
-         *
-         * @param name The name of the tests.
-         * @return The test suite.
-         */
-        public static Test suite(String name) {
-            return new TestSuite(JtdsDataSourceUnitTest.Test_JtdsDataSource_fields.class, name);
-        }
-
-        /**
-         * Default constructor.
-         * <p/>
-         * This class only has one default setup (SQL Server with TDS 7.0),
-         * so flags are set to make sure only that configuration is tested.
-         */
-        public Test_JtdsDataSource_fields() {
-
-            setOnlySqlServerTests(true);
-            setOnlyTds70Tests(true);
-
-            setTester(
-                    new DefaultPropertiesTester() {
-
-                        public void assertDefaultProperty(
-                                String message, String url, Properties properties, String fieldName,
-                                String key, String expected) {
-
-                            // Hack for JtdsDataSource.getTds()
-                            {
-                                if ("tdsVersion".equals(fieldName)) {
-                                    fieldName = "tds";
-                                }
-                            }
-
-                            JtdsDataSource dataSource = new JtdsDataSource();
-                            String actual =
-                                    String.valueOf(
-                                            invokeInstanceMethod(
-                                                    dataSource,
-                                                    "get" + ucFirst(fieldName),
-                                                    new Class[]{}, new Object[]{}));
-                            assertEquals(message, expected, actual);
-                        }
-                    }
-            );
-        }
-    }
-
-
-
-    /**
-     * Class used to test {@link JtdsDataSource#getReference()}.
-     */
-    public static class Test_JtdsDataSource_getReference
-            extends DefaultPropertiesTestLibrary {
-
-        /**
-         * Construct a test suite for this library.
-         *
-         * @param name The name of the tests.
-         * @return The test suite.
-         */
-        public static Test suite(String name) {
-            return new TestSuite(
-                    JtdsDataSourceUnitTest.Test_JtdsDataSource_getReference.class, name);
-        }
-
-        /**
-         * Default constructor.
-         * <p/>
-         * This class only has one default setup (SQL Server with TDS 7.0),
-         * so flags are set to make sure only that configuration is tested.
-         */
-        public Test_JtdsDataSource_getReference() {
-            setOnlySqlServerTests(true);
-            setOnlyTds70Tests(true);
-            setTester(
-                    new DefaultPropertiesTester() {
-
-                        public void assertDefaultProperty(
-                                String message, String url, Properties properties, String fieldName,
-                                String key, String expected) {
-
-                            try {
-                                Reference reference = new JtdsDataSource().getReference();
-                                assertEquals(message, expected, reference.get(Messages.get(key)).getContent());
-                            }
-                            catch (NamingException e) {
-                                throw new RuntimeException(e.getMessage());
-                            }
-                        }
-                    }
-            );
-        }
     }
 
     public static class Test_JtdsDataSource_getConnection extends UnitTestBase {
--- src/test/net/sourceforge/jtds/test/JtdsObjectFactoryUnitTest.java
+++ src/test/net/sourceforge/jtds/test/JtdsObjectFactoryUnitTest.java
@@ -2,52 +2,26 @@ package net.sourceforge.jtds.test;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import net.sourceforge.jtds.jdbcx.JtdsDataSource;
+
 import net.sourceforge.jtds.jdbcx.JtdsObjectFactory;
-import javax.naming.Reference;
-import java.util.Properties;
 
 
 
 /**
  * Unit tests for the {@link JtdsObjectFactory} class.
- * 
+ *
  * @author David D. Kilzer
- * @version $Id: JtdsObjectFactoryUnitTest.java,v 1.3 2004-08-30 15:44:23 bheineman Exp $
+ * @version $Id: JtdsObjectFactoryUnitTest.java,v 1.4 2005-02-07 13:47:45 alin_sinpalean Exp $
  */
 public class JtdsObjectFactoryUnitTest extends UnitTestBase {
-
-    /**
-     * Construct a test suite for this class.
-     * <p/>
-     * The test suite includes the tests in this class, and adds tests from {@link DefaultPropertiesTestLibrary} after
-     * creating an anonymous {@link DefaultPropertiesTester} object.
-     * 
-     * @return The test suite to run.
-     */
-    public static Test suite() {
-
-        final TestSuite testSuite = new TestSuite(JtdsObjectFactoryUnitTest.class);
-
-        testSuite.addTest(
-                JtdsObjectFactoryUnitTest.Test_JtdsObjectFactory_getObjectInstance.suite(
-                        "test_getObjectInstance_DefaultProperties"));
-
-        // todo: Run through tests will all NON-DEFAULT properties (JtdsObjectFactory is missing some properties)
-
-        return testSuite;
-    }
-
-
     /**
      * Constructor.
-     * 
+     *
      * @param name The name of the test.
      */
     public JtdsObjectFactoryUnitTest(String name) {
         super(name);
     }
-
 
     /**
      * Tests that the public constructor works.
@@ -56,67 +30,5 @@ public class JtdsObjectFactoryUnitTest extends UnitTestBase {
      */
     public void testPublicConstructor() {
         assertNotNull(new JtdsObjectFactory());
-    }
-
-
-
-    /**
-     * Class used to test {@link JtdsObjectFactory#getObjectInstance(Object, javax.naming.Name, javax.naming.Context,
-            * java.util.Hashtable)}.
-     */
-    public static class Test_JtdsObjectFactory_getObjectInstance
-            extends DefaultPropertiesTestLibrary {
-
-        /**
-         * Construct a test suite for this library.
-         * 
-         * @param name The name of the tests.
-         * @return The test suite.
-         */
-        public static Test suite(String name) {
-            return new TestSuite(JtdsObjectFactoryUnitTest.Test_JtdsObjectFactory_getObjectInstance.class, name);
-        }
-
-
-        /**
-         * Default constructor.
-         * <p/>
-         * This class only has one default setup (SQL Server with TDS 7.0), so flags are set to make sure only that
-         * configuration is tested.
-         */
-        public Test_JtdsObjectFactory_getObjectInstance() {
-            setOnlySqlServerTests(true);
-            setOnlyTds70Tests(true);
-            setTester(
-                    new DefaultPropertiesTester() {
-
-                        public void assertDefaultProperty(
-                                String message, String url, Properties properties, String fieldName,
-                                String key, String expected) {
-
-                            // Hack for JtdsDataSource.getTds()
-                            {
-                                if ("tdsVersion".equals(fieldName)) {
-                                    fieldName = "tds";
-                                }
-                            }
-
-                            try {
-                                Reference reference = new JtdsDataSource().getReference();
-                                JtdsDataSource dataSource =
-                                        (JtdsDataSource) new JtdsObjectFactory().getObjectInstance(
-                                                reference, null, null, null);
-                                String actual =
-                                        String.valueOf(invokeInstanceMethod(dataSource,
-                                                                            "get" + ucFirst(fieldName),
-                                                                            new Class[]{}, new Object[]{}));
-                                assertEquals(message, expected, actual);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e.getMessage());
-                            }
-                        }
-                    }
-            );
-        }
     }
 }

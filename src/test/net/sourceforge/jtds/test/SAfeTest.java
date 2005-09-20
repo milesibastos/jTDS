3bf10882c7c548fb07fb1fc31ff1e7f72ea0fb35
ddkilzer <ddkilzer@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2005-09-20 18:03:29 +0000

    Prevented NullPointerException when after adding failure message output to an assertNull() method call in testMaxFieldSize().
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@869 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/test/net/sourceforge/jtds/test/SAfeTest.java
+++ src/test/net/sourceforge/jtds/test/SAfeTest.java
@@ -19,7 +19,7 @@ import java.util.Vector;
 
 /**
  * @author  Alin Sinpalean
- * @version $Id: SAfeTest.java,v 1.57 2005-09-20 18:01:17 ddkilzer Exp $
+ * @version $Id: SAfeTest.java,v 1.58 2005-09-20 18:03:29 ddkilzer Exp $
  * @since   0.4
  */
 public class SAfeTest extends DatabaseTestCase {
@@ -1666,7 +1666,8 @@ public class SAfeTest extends DatabaseTestCase {
         stmt.setMaxFieldSize(3);
         rs = stmt.executeQuery("select * from #testMaxFieldSize");
         assertNotNull(rs);
-        assertNull(stmt.getWarnings().getMessage(), stmt.getWarnings());
+        final SQLWarning stmtWarnings = stmt.getWarnings();
+        assertNull(stmtWarnings == null ? "" : stmtWarnings.getMessage(), stmtWarnings);
         assertNull(rs.getWarnings());
         assertTrue(rs.next());
         assertEquals(1, rs.getInt(1));

f4c9bf5ac639c96edc8929626028330415444c3a
ddkilzer <ddkilzer@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2005-09-22 17:20:13 +0000

    Added copyright to top of file.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@882 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/test/net/sourceforge/jtds/test/JtdsObjectFactoryUnitTest.java
+++ src/test/net/sourceforge/jtds/test/JtdsObjectFactoryUnitTest.java
@@ -1,3 +1,20 @@
+//jTDS JDBC Driver for Microsoft SQL Server and Sybase
+//Copyright (C) 2004 The jTDS Project
+//
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+//
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this library; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
 package net.sourceforge.jtds.test;
 
 import javax.naming.Reference;
@@ -11,7 +28,7 @@ import net.sourceforge.jtds.jdbc.Driver;
  *
  * @author David D. Kilzer
  * @author Alin Sinpalean
- * @version $Id: JtdsObjectFactoryUnitTest.java,v 1.6 2005-03-04 00:11:11 alin_sinpalean Exp $
+ * @version $Id: JtdsObjectFactoryUnitTest.java,v 1.7 2005-09-22 17:20:13 ddkilzer Exp $
  */
 public class JtdsObjectFactoryUnitTest extends UnitTestBase {
     /**

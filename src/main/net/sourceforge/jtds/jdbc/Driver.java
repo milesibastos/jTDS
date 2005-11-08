2d69cb50193e437fecb05b9cec52528e544fc641
alin_sinpalean <alin_sinpalean@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2005-11-08 12:04:28 +0000

    o Updated version to 1.2.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@901 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbc/Driver.java
+++ src/main/net/sourceforge/jtds/jdbc/Driver.java
@@ -45,7 +45,7 @@ import net.sourceforge.jtds.ssl.Ssl;
  * @author Brian Heineman
  * @author Mike Hutchinson
  * @author Alin Sinpalean
- * @version $Id: Driver.java,v 1.64 2005-10-27 13:22:33 alin_sinpalean Exp $
+ * @version $Id: Driver.java,v 1.65 2005-11-08 12:04:28 alin_sinpalean Exp $
  */
 public class Driver implements java.sql.Driver {
     /** URL prefix used by the driver (i.e <code>jdbc:jtds:</code>). */
@@ -53,7 +53,7 @@ public class Driver implements java.sql.Driver {
     /** Driver major version. */
     static final int MAJOR_VERSION = 1;
     /** Driver minor version. */
-    static final int MINOR_VERSION = 1;
+    static final int MINOR_VERSION = 2;
     /** Driver version miscellanea (e.g "-rc2", ".1" or <code>null</code>). */
     static final String MISC_VERSION = null;
     /** Set if the JDBC specification to implement is 3.0 or greater. */

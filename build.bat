b845f12a020beb34195a68c0fc2c7952b7602975
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2007-07-08 16:52:37 +0000

    o [1567332] Updated build file to work with or without backslashes on    Windows.  This also fixes the problem of having spaces in the JAVA_HOME    path.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@941 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- build.bat
+++ build.bat
@@ -2,11 +2,15 @@
 
 if "%JAVA_HOME%" == "" goto error
 
+set _javatemp=%JAVA_HOME%
+
 echo.
 echo Building jTDS...
 echo ----------------
 
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar
+IF NOT %JAVA_HOME:~-1% == \ set JAVA_HOME=%JAVA_HOME%\
+
+set LOCALCLASSPATH=%JAVA_HOME%lib\tools.jar
 REM set LOCALCLASSPATH=%LOCALCLASSPATH%;%ANT_HOME%\lib\ant.jar
 REM set LOCALCLASSPATH=%LOCALCLASSPATH%;%ANT_HOME%\lib\optional.jar
 for %%i in (lib\*.jar) do call lcp.bat %%i
@@ -17,7 +21,7 @@ echo Building with classpath %LOCALCLASSPATH%
 echo.
 echo Starting Ant...
 
-"%JAVA_HOME%\bin\java.exe" -Dant.home="." -classpath "%LOCALCLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+"%JAVA_HOME%bin\java.exe" -Dant.home="." -classpath "%LOCALCLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
 
 goto end
 
@@ -32,3 +36,4 @@ echo "location of the Java Virtual Machine you want to use."
 
 set LOCALCLASSPATH=
 set ANT_HOME=
+set JAVA_HOME=%_javatemp%

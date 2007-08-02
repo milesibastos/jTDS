d7b2ec7ef52ef42ec7603112a0ddb090be4f2384
bheineman <bheineman@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2007-08-02 22:35:28 +0000

    o Corrected bug [1765905] Driver version miscellanea is incorrect;    updated to 1.2.2.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/trunk/jtds@967 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbc/Driver.java
+++ src/main/net/sourceforge/jtds/jdbc/Driver.java
@@ -45,7 +45,7 @@ import net.sourceforge.jtds.ssl.Ssl;
  * @author Brian Heineman
  * @author Mike Hutchinson
  * @author Alin Sinpalean
- * @version $Id: Driver.java,v 1.69 2007-07-08 21:38:13 bheineman Exp $
+ * @version $Id: Driver.java,v 1.70 2007-08-02 22:35:27 bheineman Exp $
  */
 public class Driver implements java.sql.Driver {
     /** URL prefix used by the driver (i.e <code>jdbc:jtds:</code>). */
@@ -55,7 +55,7 @@ public class Driver implements java.sql.Driver {
     /** Driver minor version. */
     static final int MINOR_VERSION = 2;
     /** Driver version miscellanea (e.g "-rc2", ".1" or <code>null</code>). */
-    static final String MISC_VERSION = null;
+    static final String MISC_VERSION = ".2";
     /** Set if the JDBC specification to implement is 3.0 or greater. */
     public static final boolean JDBC3 =
             "1.4".compareTo(System.getProperty("java.specification.version")) <= 0;

a27e015f338a3f1729da27603627223f6266cc8c
ickzon <ickzon@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2013-07-17 06:08:58 +0000

    o CachedResultSet now releases occupied TdsCore instances in its close() method.
    git-svn-id: http://svn.code.sf.net/p/jtds/code/branches/jTDS%201.3%20(stable)@1253 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/main/net/sourceforge/jtds/jdbc/CachedResultSet.java
+++ src/main/net/sourceforge/jtds/jdbc/CachedResultSet.java
@@ -75,9 +75,9 @@ public class CachedResultSet extends JtdsResultSet {
     /** True if this is a local temporary result set. */
     protected final boolean tempResultSet;
     /** Cursor TdsCore object. */
-    protected final TdsCore cursorTds;
+    protected TdsCore cursorTds;
     /** Updates TdsCore object used for positioned updates. */
-    protected final TdsCore updateTds;
+    protected TdsCore updateTds;
     /** Flag to indicate Sybase. */
     protected boolean isSybase;
     /** Fetch size has been changed. */
@@ -172,8 +172,6 @@ public class CachedResultSet extends JtdsResultSet {
         pos           = POS_BEFORE_FIRST;
         tempResultSet = true;
         cursorName    = null;
-        cursorTds     = null;
-        updateTds     = null;
         procName      = null;
         procedureParams = null;
     }
@@ -220,8 +218,6 @@ public class CachedResultSet extends JtdsResultSet {
         pos           = POS_BEFORE_FIRST;
         tempResultSet = true;
         cursorName    = null;
-        cursorTds     = null;
-        updateTds     = null;
         procName      = null;
         procedureParams = null;
         //
@@ -254,8 +250,6 @@ public class CachedResultSet extends JtdsResultSet {
         tempResultSet = true;
         cursorName    = null;
         rowData.add(copyRow(data));
-        cursorTds     = null;
-        updateTds     = null;
         procName      = null;
         procedureParams = null;
     }
@@ -1024,16 +1018,24 @@ public class CachedResultSet extends JtdsResultSet {
          }
      }
 
-     public void close() throws SQLException {
-         if (!closed) {
-             try {
-                 cursorClose();
-             } finally {
-                 closed    = true;
-                 statement = null;
-             }
+   public void close()
+      throws SQLException
+   {
+      if( ! closed )
+      {
+         try
+         {
+            cursorClose();
          }
-     }
+         finally
+         {
+            closed    = true;
+            statement = null;
+            cursorTds = null;
+            updateTds = null;
+         }
+      }
+   }
 
      public void deleteRow() throws SQLException {
          checkOpen();
